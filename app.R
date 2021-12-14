730e91c8d0b5c47a33c28c673969d458eb78042a
Yannick Wartmann IRM Laptop <yannickwartmann@gmail.com>
2021-12-14 14:43:31 +0100

    dropdown and if/else calculation based on that works

--- app.R
+++ app.R
@@ -1,17 +1,25 @@
+
+# libraries ---------------------------------------------------------------
+
+
 library(shiny)
 library(tidyverse)
 library(webchem)
 
-# Define UI for app that draws a histogram ----
+
+# ui ----------------------------------------------------------------------
+
 ui <- fluidPage(
   
-  # App title ----
+  #App Title
   titlePanel("Molarity converter"),
   
-  # Sidebar layout with input and output definitions ----
+  # sidebar layout ----------------------------------------------------------
+
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
+    # sidebarpanel ------------------------------------------------------------
+
     sidebarPanel(
       fluidRow(
         column(width=12,
@@ -34,79 +42,90 @@ ui <- fluidPage(
         )
       )
     ),
+
+    # mainPanel ---------------------------------------------------------------
     
-    # Main panel for displaying outputs ----
     mainPanel(
-      
-      # Output: table ----
       textOutput("error"),
       tableOutput("table")
-      
-      
     )
   )
 )
 
 
-# Define server logic required to draw a histogram ----
+# server ------------------------------------------------------------------
+
+
 server <- function(input, output) {
+  
+  # variable/reactive values definition -------------------------------------
   
   units <- c("M","muM","mug/L","g/L")
   rv <- reactiveValues()
   rv$data <- NULL
-  text <- reactiveValues()
-  text$data <- NULL
-  
+
+  # data generation ---------------------------------------------------------
+
   
   data <- observeEvent(input$calculate, {
     
+    
+    # check for inputs --------------------------------------------------------
     req(
       isTruthy(input$substance),
-      isTruthy(input$concM) || isTruthy(input$concMu)
-    )
-    if(isTruthy(input$concM) & isTruthy(input$concMu)){
-      stop <- NULL
-      text$data <- "You have supplied to concentration inputs, can not calculate"
-      print("Two values supplied, logical error")
-    }else{
-      stop <- TRUE
-      text$data <- NULL
-    }
-    req(stop)
+      isTruthy(input$conc))
+    
     print("Event started")
+
+    # pubchem query -----------------------------------------------------------
     
     query <- cir_query(input$substance,"mw")
     query <- enframe(query)
     names(query) <- c("Substance","MW")
     query$MW <- as.numeric(query$MW)
     
-    if(input$concM!=""){
-      concM <- as.numeric(input$concM)
+    # calculations, based on selectorInput ------------------------------------
+
+    if(input$unit==units[1]){
+      concM <- as.numeric(input$conc)
       result <- (concM*query$MW)*1000
-      frame <- data.frame(M=concM,mugL=result,gL=result/1000)
+      frame <- data.frame(M=concM,muM=concM*1000,mugL=result,gL=result/1000)
+      frame <- bind_cols(query,frame)
+      
+    }else if(input$unit==units[2]){
+      concmuM <- as.numeric(input$conc)
+      result <- ((concmuM/1000)*query$MW)*1000
+      frame <- data.frame(M=concmuM/1000,muM=concmuM,mugL=result,gL=result/1000)
+      frame <- bind_cols(query,frame)
+      
+    }else if(input$unit==units[3]){
+      concMu <- as.numeric(input$conc)
+      result <- (concMu/1000)/query$MW
+      frame <- data.frame(M=result,muM=result*1000,mugL=concMu,gL=concMu/1000)
       frame <- bind_cols(query,frame)
       
     }else{
-      concMu <- as.numeric(input$concMu)
-      result <- (concMu/1000)/query$MW
-      frame <- data.frame(M=result,mugL=concMu,gL=concMu/1000)
+      concgL <- as.numeric(input$conc)
+      result <- (concgL)/query$MW
+      frame <- data.frame(M=result,muM=result*1000,mugL=concgL*1000,gL=concgL)
       frame <- bind_cols(query,frame)
     }
-    
+
+    # addition of data to exisiting frame or new frame ------------------------
+
   if(is.null(rv$data)){
     rv$data <- frame
   }else{
     rv$data <- bind_rows(rv$data,frame)
   }
   
-  }
-  )
+}
+)
+
+  # output rendering --------------------------------------------------------
   
   output$table <- renderTable(
     rv$data,digits=5
-  )
-  output$error <- renderText(
-    text$data
   )
   
 }
