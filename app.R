f22554c19846ee2e361f73cbc118d22795d35b08
Yannick Wartmann <yannickwartmann@gmail.com>
2021-12-16 20:09:17 +0100

    Multiple hits are correctly calculated as #1 and #2, additionally there is an exit now for when no compound was found

--- app.R
+++ app.R
@@ -46,7 +46,7 @@ ui <- fluidPage(
     # mainPanel ---------------------------------------------------------------
     
     mainPanel(
-      textOutput("error"),
+      textOutput("wrong"),
       tableOutput("table")
     )
   )
@@ -64,6 +64,8 @@ server <- function(input, output) {
   
   rv <- reactiveValues()
   rv$data <- NULL
+  wrong <- reactiveValues()
+  wrong$data <- NULL
   
   # data generation ---------------------------------------------------------
 
@@ -119,6 +121,35 @@ server <- function(input, output) {
         placeholder <<- x
       }
     }
+    calcMulti <- function(name,unit,MW,conc){
+      
+      if(unit==units[1]){
+        concM <- as.numeric(conc)
+        result <- (concM*MW)*1000
+        x <- data.frame(Substance=name,MW=MW,M=concM,mM=concM*1000,muM=concM*1e6,mugmL=result,gL=result/1000)
+        placeholder <<- x
+      }else if(unit==units[2]){
+        concmM <- as.numeric(conc)
+        result <- ((concmM/1000)*MW)*1000
+        x <- data.frame(Substance=name,MW=MW,M=concmM/1000,mM=concmM,muM=concmM*1000,mugmL=result,gL=result/1000)
+        placeholder <<- x
+      }else if(unit==units[3]){
+        concmuM <- as.numeric(conc)
+        result <- ((concmuM/1e6)*MW)*1000
+        x <- data.frame(Substance=name,MW=MW,M=concmuM/1e6,mM=concmuM/1000,muM=concmuM,mugmL=result,gL=result/1000)
+        placeholder <<- x
+      }else if(unit==units[4]){
+        concMg <- as.numeric(conc)
+        result <- (concMg/1000)/MW
+        x <- data.frame(Substance=name,MW=MW,M=result,mM=result*1000,muM=result*1e6,mugmL=concMg,gL=concMg/1000)
+        placeholder <<- x
+      }else{
+        concgL <- as.numeric(conc)
+        result <- (concgL)/MW
+        x <- data.frame(Substance=name,MW=MW,M=result,mM=result*1000,muM=result*1e6,mugmL=concgL*1000,gL=concgL)
+        placeholder <<- x
+      }
+    }
     #print("functions loaded")
     
     # pubchem query -----------------------------------------------------------
@@ -131,8 +162,8 @@ server <- function(input, output) {
     unit <- input$unit
     conc <- input$conc
     
-    if(length(query==1)){ #the normal one as before
-      
+    if (length(query)==1 & !is.na(query) ) { #the normal one as before
+      print("nono")
       query <- data.frame(Substance=str_extract(names(query)[1],"[:alpha:]+"),MW=as.numeric(query[1])) #extract the 1 row correctly
       
       #define case unique variables
@@ -140,43 +171,44 @@ server <- function(input, output) {
       
       #calc the thing as ususal
       calc(unit,MW,conc)
+      go <- 1
       
-    }else if(length(query>1)){
+    }else if(length(query)>1){
       names <- names(query)
       MW <- unname(query)
       leng <- length(names)
       query <- data.frame(Substance=names,MW=as.numeric(MW))
+      framelist <- list()
       
+      for(i in 1:leng){
+        calcMulti(names[i],unit,MW[i],conc)
+        framelist[[paste(names[i])]] <- placeholder
+      }
+      placeholder <- bind_rows(framelist)
+      go <- 1
       
+    }else if(is.na(query)){
+      print("nothing found")
+      wrong$data <- "No compound found"
+      go <- 0
+    }
+    else{
+      wrong$data <- "Something went wrong"
+      go <- 0
     }
     
-    
-    # calculations, based on selectorInput ------------------------------------
-
-    unit <- input$unit
-    #print(unit)
-    MW <- query$MW
-    #print(MW)
-    conc <- input$conc
-    #print(conc)
-
-    #print("calc started")
-    
-    calc(unit,MW,conc)
-    
-    #print("calc done")
-
     # addition of data to exisiting frame or new frame ------------------------
-    colnames(placeholder) <- c("Substance","MW",units)
-    
-  if(is.null(rv$data)){
-    rv$data <- placeholder
+  
+  if(go==1){
+    if(is.null(rv$data)){
+      rv$data <- placeholder
+    }else{
+      rv$data <- bind_rows(rv$data,placeholder)
+    }
   }else{
-    rv$data <- bind_rows(rv$data,placeholder)
+    print("No compound found")
   }
-  
-  
-}
+    }
 )
 
   # output rendering --------------------------------------------------------
@@ -184,7 +216,9 @@ server <- function(input, output) {
   output$table <- renderTable(
     rv$data,digits=5
   )
-  
+  output$wrong <- renderText(
+    wrong$data
+  )
 }
 
 shinyApp(ui = ui, server = server)
